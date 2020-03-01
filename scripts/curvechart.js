9aa493f2c8f9b23a7ebddb44db9b5bda41cce11f
Sherlock Ma <xizma@ucdavis.edu>
2020-03-01 11:38:06 -0800

    first commit

---
+++ scripts/curvechart.js
@@ -0,0 +1,17 @@
+/**
+ * initialize the chart
+ * @param {array} data the data loaded from csv file
+ * ! don't change data in any way, because it would be be passed to other charts as well.
+ */
+curvechart.init = function(data){
+
+}
+
+/**
+ * update the data using a transition
+ * @param {number} year the new year in which data need to be displayed
+ * @param {number} duration the duration of the transition
+ */
+curvechart.updateYear = function(year, duration){
+
+}
---
+++ scripts/scatterplot.js
@@ -0,0 +1,20 @@
+// all the functions, fields should be put under this object
+scatterplot = {};
+
+/**
+ * initialize the chart
+ * @param {array} data the data loaded from csv file
+ * ! don't change data in any way, because it would be be passed to other charts as well.
+ */
+scatterplot.init = function(data){
+
+}
+
+/**
+ * update the data using a transition
+ * @param {number} year the new year in which data need to be displayed
+ * @param {number} duration the duration of the transition
+ */
+scatterplot.updateYear = function(year, duration){
+
+}
